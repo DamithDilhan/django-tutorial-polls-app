bfd51f3033ff42f83856e374160e5c2e87f5d19b
Damith Dilhan <55306956+DamithDilhan@users.noreply.github.com>
2022-06-24 17:25:38 +0530

    feat: add templates

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
+    # Ex: /polls/
     path('', views.index, name='index'),
+    # Ex: /polls/5
+    path("<int:question_id>/", views.details, name="details"),
+    # Ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # Ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote")
+
 ]
